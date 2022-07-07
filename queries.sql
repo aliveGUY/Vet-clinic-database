3289347725562b85ecd0e44e9460a523bb202019
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-07 03:15:50 +0300

    done

--- queries.sql
+++ queries.sql
@@ -16,3 +16,12 @@ SELECT SUM(escape_attempts) FROM animals WHERE neutered IS true;
 SELECT MAX(weight_kg), MIN(weight_kg) FROM animals WHERE species = 'digimon';
 SELECT MAX(weight_kg), MIN(weight_kg) FROM animals WHERE species = 'pokemon';
 SELECT AVG(escape_attempts) FROM animals  WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT name FROM animals WHERE owner_id = (SELECT id FROM owners WHERE owners.full_name = 'Melody Pond');
+SELECT name FROM animals WHERE species_id = (SELECT id FROM species WHERE species.name = 'Pokemon');
+SELECT o.full_name, a.name FROM owners o LEFT JOIN animals a ON a.owner_id = o.id ORDER BY a.id, o.id;
+SELECT COUNT(name) FROM animals WHERE species_id = (SELECT id FROM species WHERE species.name = 'Pokemon');
+SELECT COUNT(name) FROM animals WHERE species_id = (SELECT id FROM species WHERE species.name = 'Digimon');
+SELECT name FROM animals WHERE species_id = (SELECT id FROM species WHERE species.name = 'Digimon') AND owner_id = (SELECT id FROM owners WHERE owners.full_name = 'Jennifer Orwell')
+SELECT name FROM animals WHERE escape_attempts IS NULL AND owner_id = (SELECT id FROM owners WHERE owners.full_name = 'Dean Winchester');
+SELECT o.full_name, COUNT(name) FROM owners o INNER JOIN animals a ON a.owner_id = o.id GROUP BY o.full_name;
--- schema.sql
+++ schema.sql
@@ -12,3 +12,19 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+    CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age INT
+ );
+
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100)
+);
+
+\d animals
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT  CONSTRAINT species_id REFERENCES species (id);
+ALTER TABLE animals ADD COLUMN owner_id INT  CONSTRAINT owner_id REFERENCES owners (id);
+
